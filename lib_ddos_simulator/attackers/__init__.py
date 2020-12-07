10b143f1ace183ec72b72c3692644d898bb335ab
jfuruness <jfuruness@gmail.com>
2020-12-07 03:15:01 -0500

    bug fix

--- lib_ddos_simulator/attackers/__init__.py
+++ lib_ddos_simulator/attackers/__init__.py
@@ -9,8 +9,6 @@ __email__ = "jfuruness@gmail.com, agorbenko97@gmail.com"
 __status__ = "Development"
 
 from .basic_attacker import Basic_Attacker, Basic_Lone_Attacker
-from .never_last_attacker import Never_Last_Attacker
-from .never_last_attacker import Never_Last_Lone_Attacker
 from .random_attacker import Fifty_Percent_Attacker
 from .random_attacker import Fifty_Percent_Lone_Attacker
 
